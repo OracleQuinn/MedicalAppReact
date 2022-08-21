9f4aa1e01bcaba02b19509793bacd873992328ca
Agnieszka Wardega <agneswardega084@gmail.com>
2022-08-21 20:42:32 +0200

    Draft of the Examination screen.

--- components/screens/Examination.tsx
+++ components/screens/Examination.tsx
@@ -1,7 +1,8 @@
 import { useIsFocused } from '@react-navigation/native';
 import React from 'react';
-import {ScrollView} from 'react-native';
-import {FAB, Portal} from 'react-native-paper';
+import {ScrollView, View} from 'react-native';
+import {FAB, List, Portal} from 'react-native-paper';
+import SelectDropdown from 'react-native-select-dropdown';
 import { styles } from '../../styles/Styles';
 
 interface IExaminationScreenProps {
@@ -10,9 +11,27 @@ interface IExaminationScreenProps {
 
 const ExaminationScreen: React.FunctionComponent<IExaminationScreenProps> = ({navigation}) => {
   const isScreenFocused = useIsFocused();
+  const eyeOpening = ["4 spontanicznie", "3 na głos", "2 na ból", "1 brak"];
+  const verbalReaction = ["5 zorientowany", "4 splątany", "3 niewłaściwe słowa", "2 niezrozumiałe dźwięki", "1 brak"];
   
   return (
     <ScrollView keyboardDismissMode='on-drag'>
+      <List.AccordionGroup>
+        <List.Accordion title="GLASGOW-COMA-SCALE" id="1" theme={{ colors: { primary: 'dodgerblue' }}} >
+          <SelectDropdown buttonStyle={styles.dropDown} defaultButtonText='Otwieranie oczu' data={eyeOpening} onSelect={(selectedItem, index) => {console.log(selectedItem, index)}} 
+            buttonTextAfterSelection={(selectedItem, index) => {return selectedItem}}
+	          rowTextForSelection={(item, index) => {return item}}/>
+            <SelectDropdown buttonStyle={styles.dropDown} defaultButtonText='Reakcja słowna' data={verbalReaction} onSelect={(selectedItem, index) => {console.log(selectedItem, index)}} 
+            buttonTextAfterSelection={(selectedItem, index) => {return selectedItem}}
+	          rowTextForSelection={(item, index) => {return item}}/>
+        </List.Accordion>
+        <List.Accordion title="RTS" id="2" theme={{ colors: { primary: 'dodgerblue' }}} >
+          
+        </List.Accordion>
+        <List.Accordion title="Układ oddechowy" id="3" theme={{ colors: { primary: 'dodgerblue' }}}>
+          
+        </List.Accordion>
+      </List.AccordionGroup>
       <Portal>
         <FAB icon="check" style={styles.fab} onPress={() => navigation.navigate("Rozpoznanie")} visible={isScreenFocused}/>
       </Portal>
